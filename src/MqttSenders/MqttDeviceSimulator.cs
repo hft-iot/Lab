c74e7768ca89cd548143fda44458b0cc5f52adbb
Florian Bader <me@florian-bader.de>
2026-05-18 16:27:06 +0200

    Adds smart home lab

--- src/MqttSenders/MqttDeviceSimulator.cs
+++ src/MqttSenders/MqttDeviceSimulator.cs
@@ -7,10 +7,14 @@ using MQTTnet;
 
 namespace MqttSenders
 {
-    public class MqttDeviceSimulator(ILogger<MqttDeviceSimulator> logger, IConfiguration configuration) : BackgroundService
+    public class MqttDeviceSimulator(
+        ILogger<MqttDeviceSimulator> logger,
+        IConfiguration configuration
+    ) : BackgroundService
     {
         private readonly ILogger<MqttDeviceSimulator> _logger = logger;
-        private readonly string _mqttHost = configuration.GetValue<string>("MqttHost") ?? "localhost";
+        private readonly string _mqttHost =
+            configuration.GetValue<string>("MqttHost") ?? "localhost";
         private readonly Random _random = new();
         private readonly MqttClientFactory _mqttFactory = new();
         private bool _heatingMode = true;
@@ -23,21 +27,30 @@ namespace MqttSenders
                 .Build();
             await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
 
+            var discoveryMessageLoop = StartDiscoveryMessageLoop(mqttClient, stoppingToken);
+
             mqttClient.ApplicationMessageReceivedAsync += HandleIncomingMessageEvent;
             await mqttClient.SubscribeAsync(
-                new MqttTopicFilterBuilder()
-                    .WithTopic("temperature/living_room/control")
-                    .Build(),
-                stoppingToken);
+                new MqttTopicFilterBuilder().WithTopic("temperature/living_room/control").Build(),
+                stoppingToken
+            );
 
             var temperature = 21f;
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                temperature = _heatingMode ? temperature + _random.NextSingle() : temperature - _random.NextSingle();
-                if(temperature > 25) _heatingMode = false;
-                if(temperature < 18) _heatingMode = true;
-                var iotMessage = new IotMessage<double>(temperature, DateTimeOffset.Now, "telemetry");
+                temperature = _heatingMode
+                    ? temperature + _random.NextSingle()
+                    : temperature - _random.NextSingle();
+                if (temperature > 25)
+                    _heatingMode = false;
+                if (temperature < 18)
+                    _heatingMode = true;
+                var iotMessage = new IotMessage<double>(
+                    temperature,
+                    DateTimeOffset.Now,
+                    "telemetry"
+                );
                 var payload = JsonSerializer.Serialize(iotMessage);
 
                 var applicationMessage = new MqttApplicationMessageBuilder()
@@ -45,7 +58,11 @@ namespace MqttSenders
                     .WithPayload(payload)
                     .Build();
 
-                _logger.LogInformation("Publishing Message at {Timestamp} with Temperature: {Temperature}", DateTimeOffset.Now, temperature);
+                _logger.LogInformation(
+                    "Publishing Message at {Timestamp} with Temperature: {Temperature}",
+                    DateTimeOffset.Now,
+                    temperature
+                );
 
                 await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
 
@@ -62,8 +79,13 @@ namespace MqttSenders
 
         private Task HandleIncomingMessageEvent(MqttApplicationMessageReceivedEventArgs e)
         {
-            _logger.LogInformation("Received message from topic: {topic}", e.ApplicationMessage.Topic);
-            var message = JsonSerializer.Deserialize<ControlMessage>(e.ApplicationMessage.ConvertPayloadToString());
+            _logger.LogInformation(
+                "Received message from topic: {topic}",
+                e.ApplicationMessage.Topic
+            );
+            var message = JsonSerializer.Deserialize<ControlMessage>(
+                e.ApplicationMessage.ConvertPayloadToString()
+            );
             if (message is null)
             {
                 _logger.LogError("Received null message");
@@ -78,6 +100,53 @@ namespace MqttSenders
         private void HandleControlMessage(ControlMessage message)
         {
             //implement this method to switch between heating and cooling mode
+        }
+
+        private async Task StartDiscoveryMessageLoop(
+            IMqttClient mqttClient,
+            CancellationToken cancellationToken
+        )
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var payload =
+                    @"
+                    {
+                        ""name"": ""TemperatureSimulator"",
+                        ""device_class"":""temperature"",
+                        ""state_topic"":""temperature/living_room"",
+                        ""unique_id"":""11C1C0DA-1551-4755-8AAD-CFB1D21FC973"",
+                        ""device"":{
+                            ""identifiers"":[
+                                ""11C1C0DA-1551-4755-8AAD-CFB1D21FC973""
+                                ],
+                            ""name"":""TemperatureSimulator""
+                        },
+                        ""value_template"": ""{{ value_json.Value | round(2) }}"",
+                        ""max"": 200,
+                        ""min"": -100,
+                        ""unit_of_measurement"": ""°C""
+                    }
+                    ";
+
+                var applicationMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic("homeassistant/sensor/11C1C0DA-1551-4755-8AAD-CFB1D21FC973/config")
+                    .WithPayload(payload)
+                    .Build();
+
+                if (mqttClient != null && mqttClient.IsConnected)
+                {
+                    _logger.LogInformation("Publishing Discovery Message");
+                    await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+                }
+                else
+                {
+                    _logger.LogInformation(
+                        "Cannot send HomeAssistant Discovery Message because client is not connected"
+                    );
+                }
+                await Task.Delay(10000, cancellationToken);
+            }
         }
     }
 }
